9da9873a299848d890d9ec830fb4dfbfb782fd54
ntorrent <ntorrent@expedia.com>
2015-08-03 09:34:16 -0700

    Draft 2 with Angular

--- TFS.RoadMap/Scripts/App/main.js
+++ TFS.RoadMap/Scripts/App/main.js
@@ -1,51 +1,135 @@
-﻿$().ready(function () {
+﻿var roadmapApp = angular.module('roadmapApp', []);
 
-    // Set callback to run when API is loaded
-    $.ajax({
-        url: "/api/roadmap"
-    }).done(function (result) {
-        var data = [];
+roadmapApp.directive('timeline', function ($timeout) {
+        return {
+            restrict: 'E', // says that this directive is only for html elements
+            replace: true,
+            template: '<div id="timeline{{$id}}"></div>',
+            link: function ($scope, $element, $attributes) {
+                if ($attributes.ngModel) {
+                    // specify options
+                    var options = {
+                        width: "100%",
+                        height: "1000px",
+                        axisOnTop: true,
+                        eventMargin: 10,  // minimal margin between events
+                        eventMarginAxis: 0, // minimal margin beteen events and the axis
+                        editable: false,
+                        showNavigation: true
+                    };
 
-        $(result).each(function (index, element) {
-            var risk = '';
-            switch (element.Risk) {
-                case '1 - High':
-                    risk = 'high';
-                    break;
-                case '2 - Medium':
-                    risk = 'medium';
-                    break;
-                case '3 - Low':
-                    risk = 'low';
-                    break;
-            default:
+                    $scope.$watch($attributes.ngModel, function (a, b) {
+                        var data = $scope.$eval($attributes.ngModel);
+                        if (data && data.length > 0) {
+                            $timeout(function () {
+                                var timeline = new links.Timeline(document.getElementById("timeline"+$scope.$id), options);
+                                timeline.draw(data);
+                            }, 0);
+                        }
+                    }, true);
+                }
             }
-
-            data.push({
-                'start': new Date(element.Start),
-                'end': new Date(element.End),
-                'content': "<div>" + element.Title + "<br><a href='" + element.Url + "' target='_blank'>" + element.Url + "</a><br>Tags: " + element.Tags + "</div>",
-                'group': element.Group,
-                'className': risk
-            });
-        });
-
-        // specify options
-        var options = {
-            width: "100%",
-            height: "1000px",
-            axisOnTop: true,
-            eventMargin: 10,  // minimal margin between events
-            eventMarginAxis: 0, // minimal margin beteen events and the axis
-            editable: false,
-            showNavigation: true
         };
-
-        // Instantiate our timeline object.
-        var timeline = new links.Timeline(document.getElementById('mytimeline'), options);
-        // Draw our timeline with the created data and options
-        timeline.draw(data);
     });
+
+roadmapApp.controller('roadmapCtl', function ($scope, roadmapService) {
+    $scope.data = [];
+
+    $scope.load = function () {
+        roadmapService.getRoadMap().then(function (result) {
+            $scope.data = [];
+
+            $(result.data).each(function (index, element) {
+                var risk = '';
+                switch (element.Risk) {
+                    case '1 - High':
+                        risk = 'high';
+                        break;
+                    case '2 - Medium':
+                        risk = 'medium';
+                        break;
+                    case '3 - Low':
+                        risk = 'low';
+                        break;
+                    default:
+                }
+
+                $scope.data.push({
+                    'start': new Date(element.Start),
+                    'end': new Date(element.End),
+                    'content': "<div>" + element.Title + "<br><a href='" + element.Url + "' target='_blank'>" + element.Url + "</a><br>Tags: " + element.Tags + "</div>",
+                    'group': element.Group,
+                    'className': risk
+                });
+            });
+        }, function(error) {});
+    };
+
+    $scope.load();
+});
+
+roadmapApp.service('roadmapService', function ($http) {
+    function getRoadMap() {
+        var request = $http({
+            method: "get",
+            url: "/api/roadmap"
+        });
+        return request;
+    }
+
+    return ({
+        getRoadMap: getRoadMap
+    });
+});
+
+//$().ready(function () {
+
+//    // Set callback to run when API is loaded
+//    $.ajax({
+//        url: "/api/roadmap"
+//    }).done(function (result) {
+//        var data = [];
+
+//        $(result).each(function (index, element) {
+//            var risk = '';
+//            switch (element.Risk) {
+//                case '1 - High':
+//                    risk = 'high';
+//                    break;
+//                case '2 - Medium':
+//                    risk = 'medium';
+//                    break;
+//                case '3 - Low':
+//                    risk = 'low';
+//                    break;
+//            default:
+//            }
+
+//            data.push({
+//                'start': new Date(element.Start),
+//                'end': new Date(element.End),
+//                'content': "<div>" + element.Title + "<br><a href='" + element.Url + "' target='_blank'>" + element.Url + "</a><br>Tags: " + element.Tags + "</div>",
+//                'group': element.Group,
+//                'className': risk
+//            });
+//        });
+
+//        // specify options
+//        var options = {
+//            width: "100%",
+//            height: "1000px",
+//            axisOnTop: true,
+//            eventMargin: 10,  // minimal margin between events
+//            eventMarginAxis: 0, // minimal margin beteen events and the axis
+//            editable: false,
+//            showNavigation: true
+//        };
+
+//        // Instantiate our timeline object.
+//        var timeline = new links.Timeline(document.getElementById('mytimeline'), options);
+//        // Draw our timeline with the created data and options
+//        timeline.draw(data);
+//    });
     
 
-});
+//});
